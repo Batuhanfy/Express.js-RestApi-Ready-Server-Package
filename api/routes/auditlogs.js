b62b9dc957ddd8d0625d8769c22f81c5c2ea69ac
Batuhanfy <137509436+Batuhanfy@users.noreply.github.com>
2024-09-13 21:29:21 +0300

    response yapısı ve auditlog eklendi

--- api/routes/auditlogs.js
+++ api/routes/auditlogs.js
@@ -1,15 +1,29 @@
 const express = require('express');
+const Response = require('../Utils/Response');
 const router = express.Router();
+const AuditLogs = require("../db/models/AuditLogs");
+
+router.post('/', async (req, res, next) => {
+   try {
+      let body = req.body;
+      let query = {}
+     if(body.begin_date && body.end_date){
+
+     }
 
 
-router.get('/:id',(req,res,next)=>{
-   res.json({
-    body: req.body,
-    params: req.params,
-    query: req.query,
-    headers:req.headers
-   });
+      let auditlogs = await AuditLogs.find(query)
+
+
+
+      res.json(Response.successResponse(auditlogs));
+   } catch (err) {
+
+      let errorResponse = Response.errorResponse(err)
+      res.status(errorResponse.code).json(errorResponse);
+
+   }
 
 })
 
-module.exports=router;
+module.exports = router;
